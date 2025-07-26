efce74b0ae60f3cd2ce18b7a764f1a5785572bbb
robssonpeter <peterrobsson@gmail.com>
2025-07-26 14:50:37 +0300

    added the education matching route

--- title_similarity_service.py
+++ title_similarity_service.py
@@ -52,3 +52,18 @@ async def skill_similarity(data: SkillSimilarityRequest):
     return {
         "best_matches": best_matches
     }
+
+@app.post("/degree-similarity")
+async def degree_similarity(data: SimilarityRequest):
+    job_embedding = model.encode(data.job_title, convert_to_tensor=True)
+    candidate_embeddings = model.encode(data.candidate_titles, convert_to_tensor=True)
+
+    similarities = util.cos_sim(job_embedding, candidate_embeddings)[0].tolist()
+    max_score = max(similarities)
+    best_match = data.candidate_titles[similarities.index(max_score)]
+
+    return {
+        "best_match": best_match,
+        "max_score": max_score,
+        "all_scores": dict(zip(data.candidate_titles, similarities))
+    }
